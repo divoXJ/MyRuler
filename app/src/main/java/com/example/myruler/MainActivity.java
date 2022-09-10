2508fb5ca9671af70f9e669f1d044b23c5739a55
yangxj <yangxj@2345.com>
2022-09-10 13:54:47 +0800

    全屏显示

--- app/src/main/java/com/example/myruler/MainActivity.java
+++ app/src/main/java/com/example/myruler/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.myruler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        RulerSurfaceView rulerSurfaceView = new RulerSurfaceView(this);
+        setContentView(rulerSurfaceView);
+
+        //去除时间和电量栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
